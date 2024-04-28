65775cdf9f8dce091d4ed1397f2dab54af702fe8
billvog <billvog04@gmail.com>
2024-04-29 00:21:10 +0300

    Removed email-password authentication We are switching to OAuth only!

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("login/", view=views.LoginAPIView.as_view()),
-  path("register/", view=views.RegisterAPIView.as_view()),
   path("logout/", view=views.LogoutAPIView.as_view()),
   path("me/", view=views.MeAPIView.as_view()),
 ]
