f6948150ea2afb5ac83960e1860a4dc017d6b161
billvog <billvog04@gmail.com>
2024-04-30 21:09:13 +0300

    Change toast bottomOffset if in tabs layout

--- expo/modules/Providers.tsx
+++ expo/modules/Providers.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "@/modules/authentication/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useSegments } from "expo-router";
+import React from "react";
 import Toast from "react-native-toast-message";
 
 const queryClient = new QueryClient();
@@ -10,12 +11,15 @@ interface ProvidersProps {
 }
 
 export const Providers: React.FC<ProvidersProps> = ({ children }) => {
+  const segments = useSegments();
+  const isInTabs = segments[0] === "(tabs)";
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <AuthProvider>{children}</AuthProvider>
       </QueryClientProvider>
-      <Toast position="bottom" bottomOffset={60} />
+      <Toast position="bottom" bottomOffset={isInTabs ? 90 : 60} />
     </>
   );
 };
