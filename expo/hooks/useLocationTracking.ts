6ad7937cffea99a304973e3055e47d43dad1a7d1
billvog <billvog04@gmail.com>
2024-05-12 23:54:40 +0300

    A try to improve location tracking triggering We now stop the tracking only on unmount. Meaning only when we close the app, or change screen.

--- expo/hooks/useLocationTracking.ts
+++ expo/hooks/useLocationTracking.ts
@@ -1,29 +1,37 @@
-import { useEffect } from "react";
 import * as Location from "expo-location";
 import * as TaskManager from "expo-task-manager";
+import { useEffect } from "react";
 
 export function useLocationTracking(taskId: string, track: boolean) {
+  // Self explanatory
+  function stopLocationTracking() {
+    TaskManager.isTaskRegisteredAsync(taskId).then((tracking) => {
+      if (tracking) {
+        console.log("Stopping location tracking...");
+        Location.stopLocationUpdatesAsync(taskId);
+      }
+    });
+  }
+
+  // Start/stop tracking when `track` is changed
   useEffect(() => {
     if (!track) {
+      stopLocationTracking();
       return;
     }
 
-    (async () => {
-      console.log("Starting location tracking...");
-      await Location.startLocationUpdatesAsync(taskId, {
-        accuracy: Location.Accuracy.Highest,
-        timeInterval: 5000,
-        distanceInterval: 5,
-      });
-    })();
-
-    return () => {
-      TaskManager.isTaskRegisteredAsync(taskId).then((tracking) => {
-        if (tracking) {
-          console.log("Stopping location tracking...");
-          Location.stopLocationUpdatesAsync(taskId);
-        }
-      });
-    };
+    console.log("Starting location tracking...");
+    Location.startLocationUpdatesAsync(taskId, {
+      accuracy: Location.Accuracy.Highest,
+      timeInterval: 5000,
+      distanceInterval: 5,
+    });
   }, [track]);
+
+  // Stop tracking on unmount, when we close the app
+  useEffect(() => {
+    return () => {
+      stopLocationTracking();
+    };
+  }, []);
 }
