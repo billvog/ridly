7e7aeb3791d73d52d2f01aa7b617d91edcef040a
billvog <billvog04@gmail.com>
2024-05-31 20:15:00 +0300

    feature(expo): show spinner if we're not ready for AR

--- expo/modules/ui/hunt/CaptureClueARScene.tsx
+++ expo/modules/ui/hunt/CaptureClueARScene.tsx
@@ -1,3 +1,4 @@
+import FullscreenSpinner from "@/modules/ui/FullscreenSpinner";
 import { LocationPoint } from "@/types/general";
 import { gpsToAR } from "@/utils/gpsToAR";
 import {
@@ -10,17 +11,23 @@ import {
 } from "@viro-community/react-viro";
 import { Viro3DPoint } from "@viro-community/react-viro/dist/components/Types/ViroUtils";
 import * as Location from "expo-location";
-import React, { useEffect, useRef, useState } from "react";
-import { StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
 
 type CaptureClueProps = {
   clueLocation: LocationPoint;
   onClueCaptured: () => void;
 };
 
-function CaptureClueARScene({ clueLocation, onClueCaptured }: CaptureClueProps) {
-  const isReady = useRef(false);
+type CaptureClueARSceneProps = CaptureClueProps & {
+  onReady: (ready: boolean) => void;
+};
 
+function CaptureClueARScene({
+  clueLocation,
+  onClueCaptured,
+  onReady,
+}: CaptureClueARSceneProps) {
   const [trackingState, setTrackingState] = useState<ViroTrackingStateConstants>();
   const [cluePosition, setCluePosition] = useState<Viro3DPoint>([0, 0, 0]);
 
@@ -41,7 +48,7 @@ function CaptureClueARScene({ clueLocation, onClueCaptured }: CaptureClueProps) 
       const arCoordinates = gpsToAR(userLocation.coords, clueLocationPoint);
       setCluePosition([arCoordinates.x, 0, arCoordinates.z]);
 
-      isReady.current = true;
+      onReady(true);
     })();
   }, []);
 
@@ -52,41 +59,57 @@ function CaptureClueARScene({ clueLocation, onClueCaptured }: CaptureClueProps) 
         ? "Tracking normal"
         : "Not tracking"
     );
+
+    // If tracking state is normal, we can render our scene.
+    onReady(trackingState === ViroTrackingStateConstants.TRACKING_NORMAL);
   }, [trackingState]);
 
   return (
     <ViroARScene onTrackingUpdated={setTrackingState}>
-      {isReady.current && (
-        <ViroNode position={cluePosition}>
-          <ViroText
-            text="Tap to CAPTURE!"
-            transformBehaviors={["billboard"]}
-            position={[0, 0.4, 0]}
-            scale={[0.3, 0.3, 0.3]}
-            style={styles.helloWorldTextStyle}
-            onClick={() => onClueCaptured()}
-          />
-          <ViroSphere scale={[0.2, 0.2, 0.2]} />
-        </ViroNode>
-      )}
+      <ViroNode position={cluePosition}>
+        <ViroText
+          text="Tap to CAPTURE!"
+          transformBehaviors={["billboard"]}
+          position={[0, 0.4, 0]}
+          scale={[0.3, 0.3, 0.3]}
+          style={styles.captureClueTextStyle}
+          onClick={() => onClueCaptured()}
+        />
+        <ViroSphere scale={[0.2, 0.2, 0.2]} />
+      </ViroNode>
     </ViroARScene>
   );
 }
 
 export default (props: CaptureClueProps) => {
+  const [isReady, setIsReady] = useState(false);
+
   return (
-    <ViroARSceneNavigator
-      autofocus={true}
-      initialScene={{
-        scene: () => CaptureClueARScene(props),
-      }}
-      style={{ flex: 1 }}
-    />
+    <>
+      <ViroARSceneNavigator
+        autofocus={true}
+        initialScene={{
+          scene: () =>
+            CaptureClueARScene({ ...props, onReady: (ready) => setIsReady(ready) }),
+        }}
+        style={{ flex: 1 }}
+      />
+
+      {/* Render loading spinner if we're not ready. */}
+      {!isReady && (
+        <View
+          className="absolute w-full h-full"
+          style={{ backgroundColor: "rgba(0,0,0,0.6)" }}
+        >
+          <FullscreenSpinner />
+        </View>
+      )}
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  helloWorldTextStyle: {
+  captureClueTextStyle: {
     fontFamily: "Arial",
     fontWeight: "900",
     fontSize: 30,
