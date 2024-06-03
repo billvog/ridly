fb15ef4c3d5f5fd19e748deacae05778bb5cd475
billvog <billvog04@gmail.com>
2024-06-03 16:44:13 +0300

    backend: updated root urls.py to server openapi schema

--- backend/ridl_api/urls.py
+++ backend/ridl_api/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from drf_spectacular.views import (
+  SpectacularAPIView,
+  SpectacularSwaggerView,
+  SpectacularRedocView,
+)
 
 urlpatterns = [
   path("admin/", admin.site.urls),
@@ -25,3 +31,23 @@ urlpatterns = [
   path("event/", include("event.urls")),
   path("hunt/", include("hunt.urls")),
 ]
+
+# If in debug mode, add the schema and swagger ui
+if settings.DEBUG:
+  urlpatterns += [
+    path(
+      "api/schema/",
+      SpectacularAPIView.as_view(),
+      name="schema",
+    ),
+    path(
+      "api/schema/swagger-ui/",
+      SpectacularSwaggerView.as_view(url_name="schema"),
+      name="swagger-ui",
+    ),
+    path(
+      "api/schema/redoc/",
+      SpectacularRedocView.as_view(url_name="schema"),
+      name="redoc",
+    ),
+  ]
