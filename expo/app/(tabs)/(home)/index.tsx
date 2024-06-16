9a376707b85a596e80ff4ddcb180f23ad63928fa
billvog <billvog04@gmail.com>
2024-06-16 16:40:48 +0300

    feat(expo): filters on upcoming events feed

--- expo/app/(tabs)/(home)/index.tsx
+++ expo/app/(tabs)/(home)/index.tsx
@@ -1,17 +1,21 @@
 import { useIsRefreshing } from "@/hooks/useIsRefreshing";
 import { useAuth } from "@/modules/authentication/AuthContext";
 import ErrorMessage from "@/modules/ui/ErrorMessage";
-import EventScrollFeed from "@/modules/ui/EventScrollFeed";
+import EventScrollFeed, { EventFeedFilters } from "@/modules/ui/EventScrollFeed";
 import FullscreenSpinner from "@/modules/ui/FullscreenSpinner";
 import { useUpcomingEvents } from "@/types/gen";
-import React from "react";
-import { RefreshControl, ScrollView } from "react-native";
+import { useState } from "react";
+import { View } from "react-native";
 
 export default function Page() {
   const auth = useAuth();
 
+  const [feedFilters, setFeedFilters] = useState<EventFeedFilters>({
+    distance: 10,
+  });
+
   const eventsQuery = useUpcomingEvents(
-    { distance: 10 },
+    feedFilters,
     // Wait for user's location update to finish, as we want to show events nearby, if possible.
     { query: { enabled: typeof auth.didUpdateLocation === "boolean" } }
   );
@@ -23,18 +27,23 @@ export default function Page() {
   }
 
   return (
-    <ScrollView
-      refreshControl={
-        <RefreshControl refreshing={areEventsRefreshing} onRefresh={refreshEvents} />
-      }
-    >
-      {eventsQuery.isSuccess ? (
-        <EventScrollFeed title="Upcoming Events" events={eventsQuery.data} />
-      ) : (
+    <View className="flex-1">
+      {eventsQuery.isSuccess && (
+        <EventScrollFeed
+          title="Upcoming Events"
+          events={eventsQuery.data}
+          filters={feedFilters}
+          onUpdateFilters={setFeedFilters}
+          isRefreshing={areEventsRefreshing}
+          refresh={refreshEvents}
+        />
+      )}
+
+      {eventsQuery.isError && (
         <ErrorMessage viewStyle="p-10 w-full" textStyle="text-lg text-center">
           Something went wrong loading upcoming events.
         </ErrorMessage>
       )}
-    </ScrollView>
+    </View>
   );
 }
