b9a05e85215172b75180b811023038a49187949b
billvog <billvog04@gmail.com>
2024-06-22 19:34:53 +0300

    backend: changed response for user/profile retrieve view

--- backend/user/profile/serializers.py
+++ backend/user/profile/serializers.py
@@ -1,8 +1,5 @@
 from rest_framework import serializers
 
-from event.serializers import (
-  MiniEventSerializer,
-)
 from user.serializers import PublicUserSerializer
 from user.profile.models import UserProfile
 
@@ -15,18 +12,8 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
 class UserWithProfileSerializer(PublicUserSerializer):
   profile = UserProfileSerializer()
-  events = serializers.SerializerMethodField()
 
   class Meta(PublicUserSerializer.Meta):
     fields = PublicUserSerializer.Meta.fields + [
       "profile",
-      "events",
     ]
-
-  def get_events(self, obj) -> list[MiniEventSerializer]:
-    """
-    Get the first 3 events created by the user.
-    """
-
-    events = obj.creator.event_set.all()[:3]
-    return MiniEventSerializer(events, context=self.context, many=True).data
--- backend/user/profile/views.py
+++ backend/user/profile/views.py
@@ -1,5 +1,7 @@
+from rest_framework.response import Response
 from rest_framework.generics import RetrieveAPIView
 
+from event.serializers import MiniEventSerializer
 from user.models import User
 from user.profile.serializers import UserWithProfileSerializer
 
@@ -7,3 +9,37 @@ from user.profile.serializers import UserWithProfileSerializer
 class UserProfileAPIView(RetrieveAPIView):
   serializer_class = UserWithProfileSerializer
   queryset = User.objects.filter(is_active=True)
+
+  # Serializer class for joined events.
+  joined_events_serializer_class = MiniEventSerializer
+
+  def get_joined_event_objects(self, user):
+    """
+    Get 5 first events the user has joined.
+    """
+    return user.joined_events.all()[:5]
+
+  def get_joined_events_serializer(self, user):
+    """
+    Get user's joined events and return them serialized.
+    """
+    joined_events = self.get_joined_event_objects(user)
+    return self.joined_events_serializer_class(
+      joined_events, context=self.get_serializer_context(), many=True
+    )
+
+  def retrieve(self, request, *args, **kwargs):
+    # Get user and serialiaze
+    user = self.get_object()
+    user_serializer = self.get_serializer(user)
+
+    # Get user's joined events serializer
+    joined_events_serializer = self.get_joined_events_serializer(user)
+
+    # Return response
+    return Response(
+      {
+        "user": user_serializer.data,
+        "joined_events": joined_events_serializer.data,
+      }
+    )
